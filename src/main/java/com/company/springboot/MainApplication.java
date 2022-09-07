f68c3b7ced19c4e8453b9844be2cc79dfc96a82e
daniel <daniel@163.com>
2022-09-07 17:13:06 +0800

    master commit2

--- src/main/java/com/company/springboot/MainApplication.java
+++ src/main/java/com/company/springboot/MainApplication.java
@@ -1,4 +1,4 @@
-package com.company.springboot.controller;
+package com.company.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
