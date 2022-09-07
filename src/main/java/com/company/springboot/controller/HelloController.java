f7fef7fdb31c5f97f977780e8430d32b5c1060ec
daniel <daniel@163.com>
2022-09-07 15:44:36 +0800

    master commit

--- src/main/java/com/company/springboot/controller/HelloController.java
+++ src/main/java/com/company/springboot/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String handle01() {
-        return "Hello, Spring Boot 2!";
+        return "master, SpringBoot2!";
     }
 }
